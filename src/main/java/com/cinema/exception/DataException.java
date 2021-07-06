9b1d12db99f4788b00eaa57467353e62961aec50
Ivan Bodnariuk <vbforwork702@gmail.com>
2021-07-06 16:26:45 +0300

    Added DataException.java

---
+++ src/main/java/com/cinema/exception/DataException.java
@@ -0,0 +1,7 @@
+package com.cinema.exception;
+
+public class DataException extends RuntimeException{
+    public DataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
